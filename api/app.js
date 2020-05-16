48504bd5ddcba18e4ca14fa0cff84e0df6463716
Marve Kabakci <merve.kabakci@smail.th-koeln.de>
2020-05-16 03:36:05 +0200

    added basic error handling

--- api/app.js
+++ api/app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
+
+app.use(morgan('dev'))
+
+
+// error handling
 app.use((req, res, next) => {
-    res.status(200).json(
-        {message: 'Wow'}
-    );
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+// error
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        message: error.message
+    });
 });
 
 module.exports = app;
