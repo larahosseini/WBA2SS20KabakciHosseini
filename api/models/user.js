50734d06af622c295ac5ab2f0d06effeff8ba63f
larahosseini <lara.hosseini@smail.th-koeln.de>
2020-06-17 22:00:30 +0200

    expanded user model

--- api/models/user.js
+++ api/models/user.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
-const express = require('express');
-
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     username: {type: String, required: true, lowercase: true},
     favourite_kitchen: [
         {type: String, lowercase: true}
-    ]
+    ],
+    bookmarked_restaurants: [
+        {
+            restaurant: {
+                type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant', unique: true
+            }
+        }
+    ],
+    statistic: {type: mongoose.Schema.Types.ObjectId, ref: 'Statistic'}
 });
 
 module.exports = mongoose.model('User', userSchema);
+
